95521a4e2e25164282e351efb3d76f2572c44afe
Zodalia <m.finchbretlaender@gmail.com>
2026-03-29 21:19:02 +0200

    Added geomancer scene and definitions

--- Assets/Scripts/Combat/Data/ActionDefinition.cs
+++ Assets/Scripts/Combat/Data/ActionDefinition.cs
@@ -20,7 +20,6 @@ public sealed class ActionDefinition : ScriptableObject
     [field: SerializeField] public TargetType Targeting { get; private set; } = TargetType.SingleEnemy;
 
     [field: SerializeField] public int Cooldown { get; private set; } = 0;
-    [field: SerializeField] public int SpeedModifier { get; private set; } = 0;
 
     [field: SerializeField] public List<ResourceRequirement> ResourceRequirements { get; private set; } = new();
     [field: SerializeReference] public List<EffectConfig> Effects { get; private set; } = new();
